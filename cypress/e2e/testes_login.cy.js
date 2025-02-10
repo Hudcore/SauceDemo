0843a652bcce6d9a2d0a1e09f0450bafd0962811
Hudcore <hudcore@gmail.com>
2025-02-10 20:37:44 -0300

    Completando a suíte de testes de Login

---
+++ cypress/e2e/testes_login.cy.js
@@ -0,0 +1,48 @@
+beforeEach(() => {
+  cy.visit('https://www.saucedemo.com/');
+});
+
+describe('Testes de login', function() {
+  
+  
+  it('Login realizado com sucesso', function() {
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+    })
+
+  it ('Usuário certo e senha errada', function()
+  {
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="password"]').type('Secret_Sauce')
+    cy.get('[data-test="login-button"]').click()
+    cy.get('[data-test="error"]').contains('Epic sadface: Username and password do not match any user in this service')
+  })
+
+  
+  it ('Usuário errado e senha certa', function()
+  {
+    cy.get('[data-test="username"]').type('standard_userrr')
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+    cy.get('[data-test="error"]').contains('Epic sadface: Username and password do not match any user in this service')
+  })
+
+
+  it ('Login sem senha - não permitido', function()
+  {
+    cy.get('[data-test="username"]').type('standard_user')
+    cy.get('[data-test="login-button"]').click()
+    cy.get('[data-test="error"]').contains('Epic sadface: Password is required')
+  })
+
+
+  it ('Login sem senha - não permitido', function()
+  {
+    cy.get('[data-test="password"]').type('secret_sauce')
+    cy.get('[data-test="login-button"]').click()
+    cy.get('[data-test="error"]').contains('Epic sadface: Username is required')
+  })
+
+
+  })
